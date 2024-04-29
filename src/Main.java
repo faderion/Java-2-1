57b4026f41d33969a734cf020014217a2608138d
Faderion <faderion215@gmail.com>
2024-04-29 19:39:57 +0300

    GitIgnore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 36850 ; // стоимость билета
+        int rub = 20 ; // значение за которое начисляется одна бонусная миля
+        int miles = ticket / rub ; // расчет кол-ва бонусных миль
+        System.out.println(miles); // вывод на экран кол-ва бонусных миль
+
+
+    }
+}
